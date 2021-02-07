95f2872a484f3c7a439bab29a800010c9a59c44f
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-07 22:21:32 +0200

    .

---
+++ 2.2 fortune teller/index.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+function tellFortune(jobTitle, location, partnerName, numberOfChildren){
+
+return `You will be a ${jobTitle} in ${location} and married to
+${partnerName} with ${numberOfChildren} children`
+}
+console.log(tellFortune("Web developer" , "Jerusalem" , "Guy"
+, 4));
+
+
+
+
