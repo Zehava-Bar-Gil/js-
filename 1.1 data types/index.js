fd7943b2a96e0ae0a24f2fe15ae47bf1774e2625
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-07 21:45:16 +0200

    first commit

---
+++ 1.1 data types/index.js
@@ -0,0 +1,18 @@
+const string = "some string";
+const number = 22;
+const obj = {};
+const bool =true;
+const n = null;
+const und =undefined;
+const sy = Symbol ('&');
+
+
+console.log("string type is" , typeof string)
+console.log("number type is" , typeof number)
+console.log("obj type is" , typeof obj)
+console.log("bool type is" , typeof bool)
+console.log("n type is" , typeof n)
+console.log("und type is" , typeof und)
+console.log("sy type is" , typeof sy)
+  
+
---
+++ 2.1 functions/index.js
@@ -0,0 +1,10 @@
+'use strict';
+ 
+
+function describeCountry(country, population, capitalCity){
+return `${country} has ${population} people and its capital city is ${capitalCity}`
+}
+
+console.log(describeCountry(country="Finland" , population= "six million" , capitalCity="Helsinki"));
+
+
