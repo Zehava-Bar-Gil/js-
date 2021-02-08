8d0bfc9ae7badca9789019991845f63254b6bbe1
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-08 13:54:21 +0200

    adding 4.2

--- 4.2 grade assigner/index.js
+++ 4.2 grade assigner/index.js
@@ -1,2 +1,27 @@
 'use strict';
 
+const score = (points) => {
+
+    if (points  <= 64) {
+     
+        return "F"
+    }
+    else if (points > 64 && points <= 69) {
+        return "D"
+    }
+    else if (points > 69 && points <= 79) {
+        return "C"
+    }
+    else if (points > 79 && points <= 89) {
+        return "B"
+    } else {
+        return "A"
+    }
+
+}
+
+console.log(score(64))
+console.log(score(65))
+console.log(score(78))
+console.log(score(89))
+console.log(score(95))
