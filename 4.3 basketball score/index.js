512158ad0a82927df30a9e8bd6cd5db248f6e02d
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-08 14:11:22 +0200

    adding 4.3

---
+++ 4.3 basketball score/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Avg = (a ,b ,c) =>{
+    return (a+b+c) / 3;
+}
+
+const John = Avg( 89 , 120 , 103 );
+const Mike = Avg( 116 , 94 , 123 );
+
+console.log(John);
+console.log(Mike);
