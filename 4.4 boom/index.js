46da3dd590ff28636ab4974b8c2d5eab38733abc
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-08 19:38:52 +0200

    .

--- 4.4 boom/index.js
+++ 4.4 boom/index.js
@@ -15,7 +15,7 @@ function has7inside(number) {
     return false
 }
 
-for (let i = 1; i <= 50; i ++) {
+for (let i = 1; i <= 100; i ++) {
     if ((i % 7 === 0)  && i.toString().indexOf("7")){
     console.log("BOOM");
     }
