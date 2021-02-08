5b36a452e827647b43222aed7c9f52c13f5f5e4f
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-08 22:10:56 +0200

    .

--- 4.4 boom/index.js
+++ 4.4 boom/index.js
@@ -3,30 +3,33 @@
 let greetings = `#########
 7BOOM!
 #########`
-console.log(greetings)
+// console.log(greetings)
 
-function has7inside(number) {
-    let firstDigit = number % 10
-    let secondDigit = parseInt(number / 10)
-    let thirdDigit = parseInt(number / 100)
-    if (firstDigit === 7 || secondDigit === 7 || thirdDigit === 7) {
-        return true
-    }
-    return false
-}
+// function has7inside(number) {
+//     let firstDigit = number % 10
+//     let secondDigit = parseInt(number / 10)
+//     let thirdDigit = parseInt(number / 100)
+//     if (firstDigit === 7 || secondDigit === 7 || thirdDigit === 7) {
+//         return true
+//     }
+//     return false
+// }
 
-for (let i = 1; i <= 100; i ++) {
-    if ((i % 7 === 0)  && i.toString().indexOf("7")){
-    console.log("BOOM");
+const BOOM  = (n) => {
+    for (let i = 1; i <= 100; i++) {   
+        if ((i % 7 === 0) && (i.toString().indexOf("7")>-1)) {
+            console.log("BOOM BOOM");
+        }
+
+        else if (i % 7 === 0) {
+            console.log("BOOM");
+        }
+
+        else {
+            console.log(i)
+        }
     }
     
-    else if (i % 7 === 0) {
-    console.log("BOOM BOOM"); 
-    }
-
-    else {
-        console.log(i)
-    }
 }
-
-
+ 
+BOOM();
