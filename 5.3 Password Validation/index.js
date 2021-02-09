d1799ce82f55915aad9776219397d3bcfb171d5f
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-09 17:57:53 +0200

    .

--- 5.3 Password Validation/index.js
+++ 5.3 Password Validation/index.js
@@ -1,36 +1,37 @@
 'use strict';
 
-// function checkStrongPassword (password){
-//     let isBig = false, isSmall = 0, isNum =false;{
+function checkStrongPassword (password){
+    let isBig = false, isSmall = 0, isNum =false;
 
-//     for(let i=0 ; i < password.length ; i++)
-//     if (password[i].charCodeAt(0) > 65 && password[i].charCodeAt(0) <91) {
-//         isBig = true;
-//     }
-//     if (password[i].charCodeAt(0) > 95 && password[i].charCodeAt(0) <123) {
-//         isSmall ++;
-//     }
-//     if (password[i].charCodeAt(0) > 47 && password[i].charCodeAt(0) <58) {
-//         isNum =true;
-//     }
-   
-// }
-// if(isNums && isSmall > 1 && isBig) return true;
-// return false;
-// }
+    for(let i=0 ; i < password.length ; i++) {
 
-// console.log(checkStrongPassword("abcB11134"))
+    
+    if (password[i].charCodeAt(0) > 65 && password[i].charCodeAt(0) <91) {
+        isBig = true;
+    }
+    if (password[i].charCodeAt(0) > 95 && password[i].charCodeAt(0) <123) {
+        isSmall ++;
+    }
+    if (password[i].charCodeAt(0) > 47 && password[i].charCodeAt(0) <58) {
+        isNum =true;
+    }
+} 
+if(isNum && isSmall > 1 && isBig) return true;
+return false;
+}
 
-const passwordValidator4 = (password) => {
-    return password.lengh > 7 && /[A-Z]/.test(password)
-    ? "Very Strong"
-    : password.lengh > 7
-    ? "Strong"
-    : "Weak";
+console.log(checkStrongPassword("abcB11134"))
 
-};
+// const passwordValidator4 = (password) => {
+//     return password.lengh > 7 && /[A-Z]/.test(password)
+//     ? "Very Strong"
+//     : password.lengh > 7
+//     ? "Strong"
+//     : "Weak";
 
-console.log(passwordValidator4("1234Az1"))
+// };
+
+// console.log(passwordValidator4("1234Az1"))
 
 
 
