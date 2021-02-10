3c9b161b0a3c66d70c9fb1aa0ce700c097176298
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-11 00:26:12 +0200

    adding 7.2

--- 7.2 Object methods/index.js
+++ 7.2 Object methods/index.js
@@ -9,6 +9,11 @@ object. With the help of the ‘this’ keyword, this method will log a
 string like this to the console:
 ‘Finland has 6 million people, their mother tongue is Finnish, they
 have 3 neighbouring countries and a capital called Helsinki’.
+3. Call the ‘descrbie method’.
+4. Add a method called 'checkIsland' to the 'myCountry' object.
+This method will set a new property on the object, called 'isIsland'.
+'isIsland' will be true if there are no neighbouring countries, and
+false if there are. Use the ternary operator to set the property.
 ----------------------------------------------------------------*/
 
 let mycountry = {
