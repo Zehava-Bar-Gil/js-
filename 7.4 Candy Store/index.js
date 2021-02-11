f53e7d303d5e9111265b7c486deefb62a30ca214
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-11 19:56:16 +0200

    .

--- 7.4 Candy Store/index.js
+++ 7.4 Candy Store/index.js
@@ -1,6 +1,4 @@
-'use strict';
-
-const candyStore = {
+const CANDYSTORE = {
     candies: [
     {
     name: "mint gum",
@@ -15,5 +13,82 @@ const candyStore = {
     amount: 4
     },
     ],
-    cashRegister: 200
+    cashRegister: 200,
     }
+
+/*--------------------------------------------------------------------
+The function shoImplement the following getCandy function:
+Implement the following getCandy function:
+The function should return the candy element with the specified id.
+-------------------------------------------------------------------*/
+
+function GetCandy(obj, id)
+{
+    if (obj.candies[0].id === id)
+   {
+       return (obj.candies[0]);
+   } 
+   else
+   {
+      return (obj.candies[1]);
+   }
+}
+
+/*-----------------------------
+This function return the price:
+------------------------------*/
+
+function GetPrice(obj, id)
+{
+    if (obj.candies[0].id === id)
+   {
+       return (obj.candies[0].price);
+   } 
+   else
+   {
+      return (obj.candies[1].price);
+   }
+}
+
+
+/*----------------------------
+This function add a new candy
+-----------------------------*/
+function AddCandy(obj, name, id, price)
+{
+    obj.candies.push({name: name, id: id, price: price, amount: 1});
+}
+
+/*-------------------------------------------
+This function reduce the amout of this candy
+--------------------------------------------*/
+
+function BuyCandy(obj, id)
+{
+    for (let i = 0; i < obj.candies.length; ++i)
+    {
+        if (obj.candies[i].id === id)
+        {
+            obj.cashRegister += obj.candies[i].price;
+            obj.candies[i].amount -= 1;
+        }
+    }
+}
+
+/*---------------  GeneralFunction  ------------------*/
+
+function GeneralFunction()
+{
+    'use strict';
+
+    console.log(GetCandy(CANDYSTORE, "5hd7y"));
+
+    console.log(GetPrice(CANDYSTORE, "as12f"));
+
+    AddCandy(CANDYSTORE, 'snickers', 'ba90r', 4);
+    console.log(CANDYSTORE.candies);
+
+    BuyCandy(CANDYSTORE, '5hd7y');
+    console.log(CANDYSTORE.candies + ' ' + CANDYSTORE.cashRegister);
+
+};  GeneralFunction();
