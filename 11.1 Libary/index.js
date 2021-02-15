38f1794606b63d7485b4a55a7a2f32e1a7d18e03
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-15 23:55:15 +0200

    .

---
+++ 11.1 Libary/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var library = [
+    {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true
+    },
+    {
+    author: "Steve Jobs",
+    title: "Walter Isaacson",
+    readingStatus: true
+    },
+    {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of The Hunger Games",
+    readingStatus: false
+    }
+    ];
+
+    function getRead(library) {
+
+        library.filter((book) => {
+            return book.readingStatus === true;
+        })
+        
+        .map((book) => {
+            console.log(book);
+        })
+         
+    }
+
+    getRead(library);
