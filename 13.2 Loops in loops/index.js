6e6a219f89d7c4e99c533fbe86d7406b97e9c1f8
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-16 12:45:43 +0200

    .

---
+++ 13.2 Loops in loops/index.js
@@ -0,0 +1,15 @@
+/*6.3*/
+const arr = ["Zehava", "Does", "Not" ,"Sleep", "enough"]
+// const elementsLenght=(array)=>{
+//     let newArr=array.map(arrElement => arrElement.length);
+//     //The map() method creates a new array with the results of calling a function for every array element
+//     return newArr;
+// }
+// console.log(elementsLenght(arr));
+
+let lenghts = [], i = 0;
+while (i < arr.length) {
+    console.log(arr[i++]);
+}
+
+/*2 I like better for this task to use a while loop*/
