b99e1d1266ff744cbfdbdea918b9ab178776ac09
Zehava-Bar <zehava.bargil@gmail.com>
2021-02-24 23:21:47 +0200

    .

---
+++ 21.1 User age/index.js
@@ -0,0 +1,6 @@
+const name = document.querySelector('.name')
+const age = document.querySelector('#age');
+const btn = document.querySelector('click');
+console.log("name :",name)
+console.log("age :",age)
+console.log("bt")
